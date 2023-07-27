fe8dfb589df927672a4ce765db6d2d731441964d
buchynskiy <pbuchinskiy@gmail.com>
2023-07-27 19:49:09 +0200

    Button with handler

--- BotApplication.py
+++ BotApplication.py
@@ -35,7 +35,28 @@ Available commands:
 /polish --> Convert in Polish
 /ukrainian --> Convert in Ukrainian
 """)
-
+    
+def button(update: Update, context: CallbackContext) -> None:
+    update.callback_query.answer()
+    update.callback_query.message.edit_reply_markup(
+    reply_markup=InlineKeyboardMarkup([])
+    )
+    if update.callback_query.data == 'eng':
+        update.callback_query.message.delete()
+        update.callback_query.message.reply_text(f'''Hello {update.effective_user.first_name} \U0001F44B, I am capable of converting text to speech \U0001F50A. In order to use the function, click on /english command please.\n
+\U0001F4CC Important note: if you are going to insert a long text (over 3000 characters) --> keep in mind that telegram divides such messages into shorter ones and sends them separately.\n
+\U00002757 This is important because I can convert only the first message you send, so you will need to repeat the function /english as many times as the number of messages you have for conversion.''')
+    elif update.callback_query.data == 'pol':
+        update.callback_query.message.delete()
+        update.callback_query.message.reply_text(f'''Cześć {update.effective_user.first_name} \U0001F44B, potrafię tekst przekształcić w format audio \U0001F50A. Aby skorzystać z tej funkcji naciśnij tutaj --> /polish\n
+\U0001F4CC Ważne: jeśli planujesz wysłać dużą wiadomość tekstową (ponad 3000 liter) --> pamiętaj, że telegram dzieli takie duże wiadomości na mniejsze i są one wysyłane osobno.\n
+\U00002757 To jest istotne, ponieważ jestem w stanie przekonwertować tylko pierszą wiadomość, którą wysyłasz, dlatego będziesz musiał użyć funkcji /polish dla każdej osobnej wiadomości, wysłanej przez ciebie.''')
+    elif update.callback_query.data == 'ukr':
+        update.callback_query.message.delete()
+        update.callback_query.message.reply_text(f'''Привіт {update.effective_user.first_name} \U0001F44B, я вмію конвертувати текст в аудіо формат \U0001F50A. Щоб скористатися функцією натисни ---> /ukrainian\n
+\U0001F4CC Важливо: якщо ти плануєш надіслати дійсно довге текстове повідомлення (понад 3000 символів) --> пам\'ятай, що телеграм автоматично ділить великі повідомлення на менші і надсилає їх окремо.\n
+\U00002757 Це важливо, оскільки що я можу конвертувати тільки перше надіслане повідомлення, тому функцію /ukrainian потрібно буде повторити для кожного окремо надісланого повідомлення.''')
+    
 def main() -> None:
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
@@ -43,6 +64,7 @@ def main() -> None:
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('stop', stop))
     dp.add_handler(CommandHandler('help', help))
+    dp.add_handler(CallbackQueryHandler(button))
 
     updater.start_polling()
     updater.idle()
